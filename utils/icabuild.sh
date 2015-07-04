7b11bf54556723b0bd92895ab206890eac220ee4
George G <kreach3r@users.noreply.github.com>
2015-07-04 09:22:18 +0300

    Add Icarus building script
    Create a symbolic link to the root of the icarus android source:
    ln -s /vendor/icarus/utils/icabuild.sh icabuild.sh
    (run the above command from the root, e.g. ~/android/icarus)
    
    Then, run the script:
    cd ~/android/icarus
    . icabuild.sh

---
+++ utils/icabuild.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#A KreAch3R Script
+
+#Variables
+green=`tput setaf 2`
+reset=`tput sgr0`
+
+#sourcing envsetup
+source build/envsetup.sh
+
+#inform on Changelog
+echo "${green}Changelog is enabled (by default)${reset}"
+
+#enable Prebuilt Chromium
+export USE_PREBUILT_CHROMIUM=1
+echo "${green}Prebuilt Chromium will be used${reset}"
