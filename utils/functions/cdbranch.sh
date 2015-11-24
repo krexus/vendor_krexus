8db094b59d095e3a22e432024194fe8fd56f2f5f
George G <kreach3r@users.noreply.github.com>
2015-11-24 18:54:34 +0200

    Functions: cdbranch 0.1
    A modified cd command for git repos! (How many times did you commit in the wrong branch? ;)
    
    It is being aliased with the global 'cd' command in my buildscript (krebuild) right now, so be warned.
    
    Version: 0.1, initial release

---
+++ utils/functions/cdbranch.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#<!--
+#     Copyright (C) 2015 KreAch3R
+#
+#     Licensed under the Apache License, Version 2.0 (the "License");
+#     you may not use this file except in compliance with the License.
+#     You may obtain a copy of the License at
+#
+#          http://www.apache.org/licenses/LICENSE-2.0
+#
+#     Unless required by applicable law or agreed to in writing, software
+#     distributed under the License is distributed on an "AS IS" BASIS,
+#     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#     See the License for the specific language governing permissions and
+#     limitations under the License.
+#-->
+ 		 
+# Version: 0.1
+# dependencies: git
+# Info: Enhanced cd command for git repos
+
+function cdb() {
+	if [[ $# -eq 0 ]]; then
+		echo "cdb: ERROR, no directory specified"
+	else
+		gitfound=$( ls -a ./$1 | grep .git )
+		cd $1
+		if [ "$gitfound" ]; then
+			# echo "Success, git found"
+			git branch
+		fi
+	fi
+}
+
--- utils/krebuild.sh
+++ utils/krebuild.sh
@@ -9,6 +9,9 @@ reset=`tput sgr0`
 #sourcing envsetup
 source build/envsetup.sh
 
+# cd --> cdb alias
+alias cd='cdb'
+
 function release() {
    readarray -t vendorsetups < vendor/krexus/vendorsetup.sh
    for command in "${vendorsetups[@]}"; do
