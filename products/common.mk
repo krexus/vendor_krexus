c7b6301c4227e1c074ffad33976f2badc95ca9ad
Cl3Kener <https://github.com/Cl3Kener>
2015-11-30 03:20:55 +0200

    Change Default Alarm to Oxygen like M Factory Image
    Commit moved here from /build.

--- products/common.mk
+++ products/common.mk
@@ -9,6 +9,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.url.legal.android_privacy=http://www.google.com/intl/%s/mobile/android/basic/privacy.html \
     ro.com.google.clientidbase=android-google \
     ro.com.android.wifi-watchlist=GoogleGuest \
+    ro.config.alarm_alert=Oxygen.ogg \
     ro.setupwizard.enterprise_mode=1 \
     ro.com.android.dateformat=MM-dd-yyyy \
     ro.com.android.dataroaming=false \
