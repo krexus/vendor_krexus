6cd84e1a4b8aeee99e3e5dfb17f21028b4832a01
George G <kreach3r@users.noreply.github.com>
2015-12-11 00:17:21 +0200

    Drop deprecated VideoPackage2.mk
    https://github.com/krexus/frameworks_base/commit/db8c1655ea4af060967cdaef9c1bc74a1003cdc8

--- products/common.mk
+++ products/common.mk
@@ -52,6 +52,3 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES +=  \
     vendor/krexus/prebuilt/common/vendor/media/LMspeed_508.emd:system/vendor/media/LMspeed_508.emd \
     vendor/krexus/prebuilt/common/vendor/media/PFFprec_600.emd:system/vendor/media/PFFprec_600.emd
-
-# video files
-$(call inherit-product-if-exists, frameworks/base/data/videos/VideoPackage2.mk)
