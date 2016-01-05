971f10a4e6bc835bd55d7155baf308dce19a911a
George G <kreach3r@users.noreply.github.com>
2016-01-06 00:48:35 +0200

    mr1-r10 (MMB290-T): update fingerprints

--- products/krexus_angler.mk
+++ products/krexus_angler.mk
@@ -14,5 +14,5 @@ PRODUCT_MANUFACTURER := Huawei
 # Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=angler \
-    BUILD_FINGERPRINT=google/angler/angler:6.0.1/MMB29M/2431559:user/release-keys \
-    PRIVATE_BUILD_DESC="angler-user 6.0.1 MMB29M 2431559 release-keys"
+    BUILD_FINGERPRINT=google/angler/angler:6.0.1/MMB29P/2473553:user/release-keys \
+	PRIVATE_BUILD_DESC="angler-user 6.0.1 MMB29P 2473553 release-keys"
