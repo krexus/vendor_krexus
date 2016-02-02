fdebf6eaf91b594f58dda2fe3477d58695356d99
George G <kreach3r@users.noreply.github.com>
2016-02-02 12:10:50 +0200

    February patch: fingerprints

--- products/krexus_grouper.mk
+++ products/krexus_grouper.mk
@@ -1,5 +1,5 @@
 # Inherit AOSP device configuration for grouper.
-$(call inherit-product, device/asus/grouper/full_grouper.mk)
+$(call inherit-product, device/asus/grouper/aosp_grouper.mk)
 
 # Inherit common product files.
 $(call inherit-product, vendor/krexus/products/common.mk)
@@ -10,3 +10,9 @@ PRODUCT_BRAND := google
 PRODUCT_DEVICE := grouper
 PRODUCT_MODEL := Nexus 7
 PRODUCT_MANUFACTURER := asus
+
+# Fingerprint
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=grouper \
+    BUILD_FINGERPRINT=google/nakasi/grouper:6.0.1/MMB29Q/2480792:user/release-keys \
+    PRIVATE_BUILD_DESC="nakasi-user 6.0.1 MMB29Q 2480792 release-keys"
--- products/krexus_mako.mk
+++ products/krexus_mako.mk
@@ -14,5 +14,5 @@ PRODUCT_MANUFACTURER := LGE
 # Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=occam \
-    BUILD_FINGERPRINT=google/occam/mako:6.0.1/MMB29S/2489379:user/release-keys \
-    PRIVATE_BUILD_DESC="mako-user 6.0.1 MMB29S 2489379 release-keys"
+    BUILD_FINGERPRINT=google/occam/mako:6.0.1/MMB29Q/2480792:user/release-keys \
+    PRIVATE_BUILD_DESC="mako-user 6.0.1 MMB29Q 2480792 release-keys"
