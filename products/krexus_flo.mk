671b5bae426e28484175457568b105b9d917a47b
gtsiamp <gtsiamp@gmail.com>
2017-02-02 04:20:24 +0200

    products: Add flo - Nexus 7 (2013)
    Used hammerhead bootanimation

---
+++ products/krexus_flo.mk
@@ -0,0 +1,21 @@
+# Inherit AOSP device configuration for flo
+$(call inherit-product, device/asus/flo/full_flo.mk)
+
+# Inherit common product files.
+$(call inherit-product, vendor/krexus/products/common.mk)
+
+# Inherit vendor specific product files.
+$(call inherit-product, vendor/krexus/products/vendorless.mk)
+
+# Setup device specific product configuration.
+PRODUCT_NAME := krexus_flo
+PRODUCT_BRAND := google
+PRODUCT_DEVICE := flo
+PRODUCT_MODEL := Nexus 7
+PRODUCT_MANUFACTURER := asus
+
+# Fingerprint
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=razor \
+    BUILD_FINGERPRINT=google/razor/flo:7.1.1/NMF26V/3562348:user/release-keys \
+    PRIVATE_BUILD_DESC="razor-user 7.1.1 NMF26V 3562348 release-keys"
