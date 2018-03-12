cded34b4ffd7b7d29a31f162729331d94198fbc0
George G <kreach3r@gmail.com>
2018-03-12 21:12:21 +0200

    Krexusify 2

--- config/common.mk
+++ config/common.mk
@@ -37,10 +37,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/gzosp/prebuilt/common/etc/init.local.rc:root/init.gzosp.rc
 
-# Copy LatinIME for gesture typing
-PRODUCT_COPY_FILES += \
-    vendor/gzosp/prebuilt/common/lib/libjni_latinimegoogle.so:system/lib/libjni_latinimegoogle.so
-
 # SELinux filesystem labels
 PRODUCT_COPY_FILES += \
     vendor/gzosp/prebuilt/common/etc/init.d/50selinuxrelabel:system/etc/init.d/50selinuxrelabel
@@ -64,23 +60,17 @@ PRODUCT_COPY_FILES += \
     vendor/gzosp/prebuilt/common/etc/init.d/90userinit:system/etc/init.d/90userinit \
     vendor/gzosp/prebuilt/common/bin/sysinit:system/bin/sysinit
 
-# Optional packages
-PRODUCT_PACKAGES += \
-    LiveWallpapersPicker
-
 # Include explicitly to work around GMS issues
 PRODUCT_PACKAGES += \
     libprotobuf-cpp-full \
     librsjni
 
-# AudioFX
+# Extra packages
 PRODUCT_PACKAGES += \
-    AudioFX
-
-# Extra Optional packages
-PRODUCT_PACKAGES += \
+    AudioFX \
     BluetoothExt \
     LatinIME \
+    LiveWallpapersPicker \
     RootlessPixelLauncher
 
 
@@ -122,41 +112,10 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGE_OVERLAYS += vendor/gzosp/overlay/common
 
-# Boot animation include
-ifneq ($(TARGET_SCREEN_WIDTH) $(TARGET_SCREEN_HEIGHT),$(space))
-
-# determine the smaller dimension
-TARGET_BOOTANIMATION_SIZE := $(shell \
-  if [ $(TARGET_SCREEN_WIDTH) -lt $(TARGET_SCREEN_HEIGHT) ]; then \
-    echo $(TARGET_SCREEN_WIDTH); \
-  else \
-    echo $(TARGET_SCREEN_HEIGHT); \
-  fi )
-
-# get a sorted list of the sizes
-bootanimation_sizes := $(subst .zip,, $(shell ls vendor/gzosp/prebuilt/common/bootanimation))
-bootanimation_sizes := $(shell echo -e $(subst $(space),'\n',$(bootanimation_sizes)) | sort -rn)
-
-# find the appropriate size and set
-define check_and_set_bootanimation
-$(eval TARGET_BOOTANIMATION_NAME := $(shell \
-  if [ -z "$(TARGET_BOOTANIMATION_NAME)" ]; then
-    if [ $(1) -le $(TARGET_BOOTANIMATION_SIZE) ]; then \
-      echo $(1); \
-      exit 0; \
-    fi;
-  fi;
-  echo $(TARGET_BOOTANIMATION_NAME); ))
-endef
-$(foreach size,$(bootanimation_sizes), $(call check_and_set_bootanimation,$(size)))
-
-ifeq ($(TARGET_BOOTANIMATION_HALF_RES),true)
+# Bootanimation (add if it exists)
+ifneq ($(wildcard vendor/gzosp/prebuilt/bootanimation/$(TARGET_DEVICE).zip),)
 PRODUCT_COPY_FILES += \
-    vendor/gzosp/prebuilt/common/bootanimation/halfres/$(TARGET_BOOTANIMATION_NAME).zip:system/media/bootanimation.zip
-else
-PRODUCT_COPY_FILES += \
-    vendor/gzosp/prebuilt/common/bootanimation/$(TARGET_BOOTANIMATION_NAME).zip:system/media/bootanimation.zip
-endif
+        vendor/gzosp/prebuilt/bootanimation/$(TARGET_DEVICE).zip:system/media/bootanimation.zip
 endif
 
 # Versioning System
@@ -186,5 +145,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Google sounds
 include vendor/gzosp/google/GoogleAudio.mk
+
+# Set Pixel blue light theme on Gboard
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.com.google.ime.theme_id=5
 
 EXTENDED_POST_PROCESS_PROPS := vendor/gzosp/tools/gzosp_process_props.py
